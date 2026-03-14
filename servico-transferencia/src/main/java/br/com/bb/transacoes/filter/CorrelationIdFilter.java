8a8753cc48a037d3c38307f2e892f799de06c616
cristian neves <crislife987@gmail.com>
2026-03-13 21:06:54 -0300

    refactor: remove some trash comments

--- servico-transferencia/src/main/java/br/com/bb/transacoes/filter/CorrelationIdFilter.java
+++ servico-transferencia/src/main/java/br/com/bb/transacoes/filter/CorrelationIdFilter.java
@@ -14,16 +14,13 @@ public class CorrelationIdFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
-        // 1. Tenta pegar do header, se não existir, gera um novo
         String correlationId = requestContext.getHeaderString(CORRELATION_ID_HEADER);
         if (correlationId == null || correlationId.isEmpty()) {
             correlationId = UUID.randomUUID().toString();
         }
 
-        // 2. "Cola o post-it" no MDC para os logs automáticos
         MDC.put(MDC_KEY, correlationId);
 
-        // 3. Garante que o ID possa ser recuperado depois pelo Service
         requestContext.setProperty(MDC_KEY, correlationId);
     }
 }
