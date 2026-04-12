cb9beeb9eccaf9dbfffb727b65ec8d3f529e6b55
cristian neves <crislife987@gmail.com>
2026-04-12 15:49:05 -0300

    fix(sonar): false positive on sonarqube for hardcoded password

--- servico-cadastro/src/main/java/br/com/bb/cadastro/dto/PessoaDTO.java
+++ servico-cadastro/src/main/java/br/com/bb/cadastro/dto/PessoaDTO.java
@@ -26,7 +26,6 @@ public class PessoaDTO {
                 "nome='" + nome + '\'' +
                 ", cpf='***.***.***-**'" +
                 ", email='[MASKED]'" +
-                ", pwd='***'" +
                 '}';
     }
 }
